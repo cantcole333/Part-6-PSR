f3e543512713764cf5f59f4ce8797750f27b8b9f
Cole Cantwell <CantCole333@GoTVDSB.ca>
2020-10-26 10:26:59 -0400

    finished PSR

--- Part 6 PSR/Form1.Designer.cs
+++ Part 6 PSR/Form1.Designer.cs
@@ -38,18 +38,32 @@
             this.lblOpp = new System.Windows.Forms.Label();
             this.btnPlay = new System.Windows.Forms.Button();
             this.lblOpponentNum = new System.Windows.Forms.Label();
+            this.lblOpponentChoice = new System.Windows.Forms.Label();
+            this.lblGame = new System.Windows.Forms.Label();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.grpPlayerChoice.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgOpponent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // radBtnRock
             // 
             this.radBtnRock.AutoSize = true;
-            this.radBtnRock.Location = new System.Drawing.Point(8, 31);
-            this.radBtnRock.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radBtnRock.Location = new System.Drawing.Point(6, 25);
             this.radBtnRock.Name = "radBtnRock";
-            this.radBtnRock.Size = new System.Drawing.Size(77, 29);
+            this.radBtnRock.Size = new System.Drawing.Size(64, 24);
             this.radBtnRock.TabIndex = 0;
             this.radBtnRock.TabStop = true;
             this.radBtnRock.Text = "Rock";
@@ -59,10 +73,9 @@
             // radBtnPaper
             // 
             this.radBtnPaper.AutoSize = true;
-            this.radBtnPaper.Location = new System.Drawing.Point(8, 68);
-            this.radBtnPaper.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radBtnPaper.Location = new System.Drawing.Point(6, 55);
             this.radBtnPaper.Name = "radBtnPaper";
-            this.radBtnPaper.Size = new System.Drawing.Size(85, 29);
+            this.radBtnPaper.Size = new System.Drawing.Size(69, 24);
             this.radBtnPaper.TabIndex = 1;
             this.radBtnPaper.TabStop = true;
             this.radBtnPaper.Text = "Paper";
@@ -72,10 +85,9 @@
             // radBtnScissors
             // 
             this.radBtnScissors.AutoSize = true;
-            this.radBtnScissors.Location = new System.Drawing.Point(8, 105);
-            this.radBtnScissors.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radBtnScissors.Location = new System.Drawing.Point(6, 85);
             this.radBtnScissors.Name = "radBtnScissors";
-            this.radBtnScissors.Size = new System.Drawing.Size(108, 29);
+            this.radBtnScissors.Size = new System.Drawing.Size(87, 24);
             this.radBtnScissors.TabIndex = 2;
             this.radBtnScissors.TabStop = true;
             this.radBtnScissors.Text = "Scissors";
@@ -88,11 +100,9 @@
             this.grpPlayerChoice.Controls.Add(this.radBtnPaper);
             this.grpPlayerChoice.Controls.Add(this.radBtnScissors);
             this.grpPlayerChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grpPlayerChoice.Location = new System.Drawing.Point(107, 194);
-            this.grpPlayerChoice.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpPlayerChoice.Location = new System.Drawing.Point(18, 248);
             this.grpPlayerChoice.Name = "grpPlayerChoice";
-            this.grpPlayerChoice.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.grpPlayerChoice.Size = new System.Drawing.Size(188, 142);
+            this.grpPlayerChoice.Size = new System.Drawing.Size(141, 115);
             this.grpPlayerChoice.TabIndex = 3;
             this.grpPlayerChoice.TabStop = false;
             this.grpPlayerChoice.Text = "Player Choice";
@@ -101,10 +111,9 @@
             // imgPlayer
             // 
             this.imgPlayer.Image = global::Part_6_PSR.Properties.Resources.Rock_Rock;
-            this.imgPlayer.Location = new System.Drawing.Point(323, 44);
-            this.imgPlayer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.imgPlayer.Location = new System.Drawing.Point(24, 66);
             this.imgPlayer.Name = "imgPlayer";
-            this.imgPlayer.Size = new System.Drawing.Size(169, 128);
+            this.imgPlayer.Size = new System.Drawing.Size(127, 176);
             this.imgPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imgPlayer.TabIndex = 4;
             this.imgPlayer.TabStop = false;
@@ -112,10 +121,9 @@
             // imgOpponent
             // 
             this.imgOpponent.Image = global::Part_6_PSR.Properties.Resources.Kevin_Paper;
-            this.imgOpponent.Location = new System.Drawing.Point(529, 384);
-            this.imgOpponent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.imgOpponent.Location = new System.Drawing.Point(313, 71);
             this.imgOpponent.Name = "imgOpponent";
-            this.imgOpponent.Size = new System.Drawing.Size(169, 122);
+            this.imgOpponent.Size = new System.Drawing.Size(167, 171);
             this.imgOpponent.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imgOpponent.TabIndex = 5;
             this.imgOpponent.TabStop = false;
@@ -124,29 +132,30 @@
             // 
             this.lblPlayer.AutoSize = true;
             this.lblPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPlayer.Location = new System.Drawing.Point(372, 11);
-            this.lblPlayer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPlayer.Location = new System.Drawing.Point(58, 29);
             this.lblPlayer.Name = "lblPlayer";
-            this.lblPlayer.Size = new System.Drawing.Size(81, 29);
+            this.lblPlayer.Size = new System.Drawing.Size(62, 24);
             this.lblPlayer.TabIndex = 6;
             this.lblPlayer.Text = "Player";
+            this.lblPlayer.Click += new System.EventHandler(this.lblPlayer_Click);
             // 
             // lblOpp
             // 
             this.lblOpp.AutoSize = true;
             this.lblOpp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOpp.Location = new System.Drawing.Point(545, 513);
-            this.lblOpp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOpp.Location = new System.Drawing.Point(343, 29);
             this.lblOpp.Name = "lblOpp";
-            this.lblOpp.Size = new System.Drawing.Size(120, 29);
+            this.lblOpp.Size = new System.Drawing.Size(95, 24);
             this.lblOpp.TabIndex = 7;
             this.lblOpp.Text = "Opponent";
+            this.lblOpp.Click += new System.EventHandler(this.lblOpp_Click);
             // 
             // btnPlay
             // 
-            this.btnPlay.Location = new System.Drawing.Point(506, 225);
+            this.btnPlay.Location = new System.Drawing.Point(189, 113);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(104, 23);
+            this.btnPlay.Size = new System.Drawing.Size(75, 27);
             this.btnPlay.TabIndex = 8;
             this.btnPlay.Text = "Click to Play";
             this.btnPlay.UseVisualStyleBackColor = true;
@@ -155,25 +164,123 @@
             // lblOpponentNum
             // 
             this.lblOpponentNum.AutoSize = true;
-            this.lblOpponentNum.Location = new System.Drawing.Point(526, 135);
+            this.lblOpponentNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOpponentNum.Location = new System.Drawing.Point(343, 285);
+            this.lblOpponentNum.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblOpponentNum.Name = "lblOpponentNum";
-            this.lblOpponentNum.Size = new System.Drawing.Size(85, 51);
+            this.lblOpponentNum.Size = new System.Drawing.Size(111, 72);
             this.lblOpponentNum.TabIndex = 9;
             this.lblOpponentNum.Text = "Rock = 1\r\nPaper = 2\r\nScissors = 3";
+            this.lblOpponentNum.Click += new System.EventHandler(this.lblOpponentNum_Click);
+            // 
+            // lblOpponentChoice
+            // 
+            this.lblOpponentChoice.AutoSize = true;
+            this.lblOpponentChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOpponentChoice.Location = new System.Drawing.Point(320, 257);
+            this.lblOpponentChoice.Name = "lblOpponentChoice";
+            this.lblOpponentChoice.Size = new System.Drawing.Size(160, 24);
+            this.lblOpponentChoice.TabIndex = 10;
+            this.lblOpponentChoice.Text = "Opponent Choice";
+            this.lblOpponentChoice.Click += new System.EventHandler(this.lblOpponentChoice_Click);
+            // 
+            // lblGame
+            // 
+            this.lblGame.AutoSize = true;
+            this.lblGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGame.Location = new System.Drawing.Point(96, 399);
+            this.lblGame.Name = "lblGame";
+            this.lblGame.Size = new System.Drawing.Size(0, 24);
+            this.lblGame.TabIndex = 11;
+            this.lblGame.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblGame.Click += new System.EventHandler(this.lblGame_Click);
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.Location = new System.Drawing.Point(189, 145);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayAgain.TabIndex = 12;
+            this.btnPlayAgain.Text = "Play Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = true;
+            this.btnPlayAgain.Click += new System.EventHandler(this.btnPlayAgain_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(178, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 75);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(46, 22);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(86, 38);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Location = new System.Drawing.Point(330, 22);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(120, 38);
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Black;
+            this.pictureBox4.Location = new System.Drawing.Point(313, 245);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(173, 52);
+            this.pictureBox4.TabIndex = 16;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Black;
+            this.pictureBox5.Location = new System.Drawing.Point(330, 273);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(136, 92);
+            this.pictureBox5.TabIndex = 17;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Black;
+            this.pictureBox6.Location = new System.Drawing.Point(76, 389);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(345, 45);
+            this.pictureBox6.TabIndex = 18;
+            this.pictureBox6.TabStop = false;
             // 
             // PSR
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(512, 461);
+            this.Controls.Add(this.grpPlayerChoice);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.lblOpponentChoice);
             this.Controls.Add(this.lblOpponentNum);
             this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.lblOpp);
             this.Controls.Add(this.lblPlayer);
             this.Controls.Add(this.imgOpponent);
             this.Controls.Add(this.imgPlayer);
-            this.Controls.Add(this.grpPlayerChoice);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.lblGame);
+            this.Controls.Add(this.pictureBox6);
             this.Name = "PSR";
             this.Text = "Rock, Paper, Scissors";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -181,6 +288,12 @@
             this.grpPlayerChoice.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imgOpponent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,6 +311,15 @@
         private System.Windows.Forms.Label lblOpp;
         private System.Windows.Forms.Button btnPlay;
         private System.Windows.Forms.Label lblOpponentNum;
+        private System.Windows.Forms.Label lblOpponentChoice;
+        private System.Windows.Forms.Label lblGame;
+        private System.Windows.Forms.Button btnPlayAgain;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
 
--- Part 6 PSR/Form1.cs
+++ Part 6 PSR/Form1.cs
@@ -13,7 +13,9 @@ namespace Part_6_PSR
 {
     public partial class PSR : Form
     {
-        Random generator = new Random(); 
+        Random generator = new Random();
+        int playerChoice;
+        int PlayAgain = 1;
         public PSR()
         {
             InitializeComponent();
@@ -27,6 +29,7 @@ namespace Part_6_PSR
         private void radBtnPaper_CheckedChanged(object sender, EventArgs e)
         {
             imgPlayer.Image = Properties.Resources.Rock_Paper;
+            playerChoice = 2;
         }
 
         private void grpPlayerChoice_Enter(object sender, EventArgs e)
@@ -37,29 +40,104 @@ namespace Part_6_PSR
         private void radBtnRock_CheckedChanged(object sender, EventArgs e)
         {
             imgPlayer.Image = Properties.Resources.Rock_Rock;
+            playerChoice = 1;
         }
 
         private void radBtnScissors_CheckedChanged(object sender, EventArgs e)
         {
             imgPlayer.Image = Properties.Resources.Rock_Scissors;
-
+            playerChoice = 3;
         }
 
         private void btnPlay_Click(object sender, EventArgs e)
         {
-            int compChoice = generator.Next(1,4);
-            if (compChoice == 1)
+            if (PlayAgain == 0)
             {
-                imgOpponent.Image = Properties.Resources.Kevin_Rock;
+                lblGame.Text = "Please press Play Again";
             }
-            else if (compChoice == 2)
-            {
-                imgOpponent.Image = Properties.Resources.Kevin_Paper;
+            else if (PlayAgain == 1)
+                {
+                int compChoice = generator.Next(1,4);
+                    if (compChoice == playerChoice)
+                    {
+                        lblGame.Text = "Tie Game!";
+                        PlayAgain = 0;
+                    }
+                    else if (compChoice == 1 && playerChoice == 2)
+                    {
+                        imgOpponent.Image = Properties.Resources.Kevin_Rock;
+                        lblOpponentChoice.Text = "Rock";
+                        lblGame.Text = "Player won! Paper beats Rock";
+                        PlayAgain = 0;
+                    }
+                    else if (compChoice == 2 && playerChoice == 1)
+                    {
+                        imgOpponent.Image = Properties.Resources.Kevin_Paper;
+                        lblOpponentChoice.Text = "Paper";
+                        lblGame.Text = "Computer won! Paper beats Rock";
+                        PlayAgain = 0;
+                     }   
+                    else if (compChoice == 3 && playerChoice == 1)
+                    {
+                        imgOpponent.Image = Properties.Resources.Kevin_Scissors;
+                        lblOpponentChoice.Text = "Scissors";
+                        lblGame.Text = "Player won! Rock beats Scissors";
+                        PlayAgain = 0;
+                    }
+                    else if (compChoice == 1 && playerChoice == 3)
+                    {
+                        imgOpponent.Image = Properties.Resources.Kevin_Scissors;
+                        lblOpponentChoice.Text = "Scissors";
+                        lblGame.Text = "Computer won! Rock beats Scissors";
+                        PlayAgain = 0;
+                    }
+                    else if (compChoice == 2 && playerChoice == 3)
+                    {
+                        imgOpponent.Image = Properties.Resources.Kevin_Scissors;
+                        lblOpponentChoice.Text = "Paper";
+                        lblGame.Text = "Player won! Scissors beats Paper";
+                        PlayAgain = 0;
+                    }
+                    else if (compChoice == 3 && playerChoice == 2)
+                    {
+                        imgOpponent.Image = Properties.Resources.Kevin_Scissors;
+                        lblOpponentChoice.Text = "Scissors";
+                        lblGame.Text = "Computer won! Scissors beats Paper";
+                        PlayAgain = 0;
+                    }
+
             }
-            else if (compChoice == 3)
-            {
-                imgOpponent.Image = Properties.Resources.Kevin_Scissors;
-            }
+            
+        }
+
+        private void lblOpponentChoice_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lblPlayer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblOpp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            PlayAgain = 1;
+            lblGame.Text = "";
+        }
+
+        private void lblOpponentNum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGame_Click(object sender, EventArgs e)
+        {
 
         }
     }
